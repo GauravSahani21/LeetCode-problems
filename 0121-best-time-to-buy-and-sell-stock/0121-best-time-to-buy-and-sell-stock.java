62c29bdc8eeee0ce1603075d3c56881936da8f2d
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-02-17 22:34:14 +0530

    Time: 1 ms (99.7%), Space: 94.7 MB (30.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i<prices.length; i++){
             low= Math.min(low,prices[i]);
             max=Math.max(max,prices[i]-low);
-            }
+        }
        
             return max;
     }
