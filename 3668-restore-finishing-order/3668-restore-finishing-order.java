5501a730295fb3ab80f02d8596b11a6abfd49d1d
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-04-26 17:33:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.java
+++ 3668-restore-finishing-order/3668-restore-finishing-order.java
@@ -4,10 +4,11 @@ class Solution {
         int[] arr = new int[friends.length];
         int k = 0;
 
-        for (int i = 0; i < order.length; i++) {
+        for (int i = 0; i <order.length; i++) {
             for (int j = 0; j < friends.length; j++) {
                 if (order[i] == friends[j]) {
-                    arr[k++] = order[i ];
+                    arr[k] = order[i ];
+                    k++;
                     break; 
                 }
             }
