62889af983e16d1ee2686889fa998ff9794c9eb1
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-04-30 19:46:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,9 +3,8 @@ class Solution {
         int []hash = new int[26];
 
         for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            int index = ch - 'a';
-            hash[index]++;
+            
+            hash[s.charAt(i) - 'a']++;
         }
         for(int i=0; i<s.length(); i++){
             if(hash[s.charAt(i) - 'a']==1){
