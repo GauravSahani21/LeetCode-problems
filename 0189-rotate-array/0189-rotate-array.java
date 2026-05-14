20c0d70c4457d2aa5cf061629821c69ea931314c
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-05-15 00:06:58 +0530

    Time: 0 ms (100%), Space: 61.3 MB (53.63%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,12 +3,12 @@ class Solution {
 
           k = k % nums.length;
 
-        roti(nums, 0, nums.length-1);
-        roti(nums, 0, k-1);
-        roti(nums, k, nums.length-1);
+        reverse(nums, 0, nums.length-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length-1);
     }
     
-    public int roti(int []nums, int low, int high){
+    public int reverse(int []nums, int low, int high){
         while(low<high){
             int temp = nums[low];
             nums[low] = nums[high];
