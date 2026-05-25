758301d8951109352338d864fee4147b2f3a64e2
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-05-26 01:21:16 +0530

    Time: 3 ms (61.14%), Space: 48.4 MB (51.07%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.java
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j]==1){
-                    arr[j]++;
+                    arr[i]++;
                 }
             }
         }
