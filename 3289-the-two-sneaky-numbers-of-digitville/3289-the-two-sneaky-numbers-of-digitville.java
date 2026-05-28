5bccf016d632562f890c009db2b817c5db9c0608
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-05-28 21:21:11 +0530

    Time: 2 ms (59.29%), Space: 47.3 MB (11.15%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int [] hash = new int[6001];
+        int [] ans = new int[2];
+        int k=0;
+
+        for(int i=0; i<nums.length; i++){
+            hash[nums[i]]++;
+
+            if(hash[nums[i]] == 2){
+                ans[k++] = nums[i];
+                
+            }
+        }
+
+            return ans;
+    }
+}
