54d6d0ca55eb4e9d9ebe47a5fd8acba094766fd0
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-06-01 21:19:49 +0530

    Time: 5 ms (96.09%), Space: 44.4 MB (69.64%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -6,6 +6,7 @@ class Solution {
 
         for (int i = n - 1; i >= 0; i -= 3) {
             sum += cost[i];
+            
             if (i - 1 >= 0)
                 sum += cost[i - 1];
         }
