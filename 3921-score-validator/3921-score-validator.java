d321633559faa78e3e945a4d5ceb97afbea7c6d2
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-06-09 23:55:14 +0530

    Time: 2 ms (91.38%), Space: 47.3 MB (61.73%) - LeetHub

--- 3921-score-validator/3921-score-validator.java
+++ 3921-score-validator/3921-score-validator.java
@@ -5,10 +5,6 @@ class Solution {
         for (int i = 0; i < events.length; i++) {
             if(ans[1] == 10) break;
             if (events[i].equals("W")) {
-                if (ans[1] == 10) {
-                    ans[0]=0;
-                    break;
-                }
                 ans[1] += 1;
             } else if (events[i].equals("WD") || events[i].equals("NB")) {
                 ans[0] += 1;
