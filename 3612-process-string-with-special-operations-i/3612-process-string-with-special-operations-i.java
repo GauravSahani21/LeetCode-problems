b130b714fb18eec87580754aaa1748136cb906a7
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-06-16 21:54:28 +0530

    Time: 53 ms (8.09%), Space: 78.2 MB (8.82%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public String processStr(String s) {
+        ArrayList<Character> ans = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
+                ans.add(s.charAt(i));
+            } else if (s.charAt(i) == '*') {
+                if (!ans.isEmpty()) {
+                    ans.remove(ans.size() - 1);
+                }
+            } else if (s.charAt(i) == '#') {
+                ans.addAll(new ArrayList<>(ans));
+            } else if (s.charAt(i) == '%') {
+                Collections.reverse(ans);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+for (char c : ans) {
+    result.append(c);
+}
+return result.toString();
+    }
+}
