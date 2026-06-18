bf7be577f13cb7548743da7bd2605e3bf6a3f0e9
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-06-18 20:01:05 +0530

    Time: 15 ms (85.98%), Space: 47.8 MB (25.1%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,24 +1,23 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int [] hash = new int[100001];
+        int[] hash = new int[100001];
 
         int ans = -1;
-        int max=0;
+        int max = 0;
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]%2==0){
-               hash[nums[i]]++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                hash[nums[i]]++;
             }
         }
-        for(int i=0; i<hash.length; i++){
-            if(hash[i]>max){
-                ans=i;
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > max) {
+                ans = i;
                 max = hash[i];
             }
             i++;
         }
 
-        
-    return ans;
+        return ans;
     }
 }
