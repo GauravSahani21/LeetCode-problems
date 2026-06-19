2e423db8ca1c7d28b3b5d6accde398c0ef1b873c
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-06-19 19:06:07 +0530

    Time: 1 ms (13.85%), Space: 45.7 MB (98.95%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-       int [] arr1 = new int[n];
-       int [] arr2 = new int[n];
-       
-       int [] ans = new int[nums.length];
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
 
-       for(int i=0; i<n; i++){
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < n; i++) {
             arr1[i] = nums[i];
-       }
+        }
 
-       for(int i=n; i<nums.length; i++){
-            arr2[i-n] = nums[i];
-       }
+        for (int i = n; i < nums.length; i++) {
+            arr2[i - n] = nums[i];
+        }
 
+        int a = 0;
+        int b = 1;
 
-        int a =0;
-        int b=1;
-
-       for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             ans[a] = arr1[i];
             ans[b] = arr2[i];
 
@@ -27,4 +26,4 @@ class Solution {
 
         return ans;
     }
-} 
+}
