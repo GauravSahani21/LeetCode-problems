6d5060f67c47e6b4b36330db577b38b011ce7aec
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-08-26 22:43:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,8 +2,8 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
@@ -13,15 +13,15 @@ class Solution {
 
         for(int i=0; i<n; i++){
             int low = 0;
-            int high=n-1;
+            int high = n-1;
 
-            while(low<high){
+            while(low < high){
                 int temp = matrix[i][low];
-                matrix[i][low] = matrix[i][high];
-                matrix[i][high] = temp;
-                
-                low++;
-                high--;
+                matrix[i][low]= matrix[high][i];
+                matrix[high][i] = temp;
+
+                low ++;
+                high --;
             }
         }
     }
