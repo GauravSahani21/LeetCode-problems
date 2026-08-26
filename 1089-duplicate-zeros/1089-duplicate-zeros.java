0fe2fa78aff4ad4fb465035c694b7b7c357f5756
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-08-26 13:21:36 +0530

    Time: 15 ms (9.65%), Space: 46.9 MB (61.65%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -7,7 +7,8 @@ class Solution {
                 for(int j=n-1; j>i; j--){
                     arr[j] = arr[j-1];
                 }
-                if(i+1<n) arr[i+1] = 0;
+                if(i+1<n) 
+                    arr[i+1] = 0;
                 i++;
             }
         }
