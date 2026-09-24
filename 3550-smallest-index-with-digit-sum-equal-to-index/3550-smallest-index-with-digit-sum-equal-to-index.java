c4809a015ac437a9ee20b7667f2ab6b9ef0fd197
Gaurav Sahani <157032302+GauravSahani21@users.noreply.github.com>
2026-09-24 08:56:32 +0530

    Time: 1 ms (99.69%), Space: 45.5 MB (59.67%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -4,18 +4,16 @@ class Solution {
             int sum = 0;
             int temp = nums[i];
 
-          
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += digit;
+                temp = temp / 10;
+            }
 
-                while (temp > 0) {
-                    int digit = temp % 10;
-                    sum += digit;
-                    temp = temp / 10;
-                }
+            if (sum == i) {
+                return i;
+            }
 
-                if (sum == i) {
-                    return i;
-                }
-            
         }
         return -1;
     }
